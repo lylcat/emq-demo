109ab642a7a5413b963030501a5deb7672df24b0
阿雨隆 <8080011+ayulong@user.noreply.gitee.com>
2021-10-27 09:50:35 +0800

    'C1再次更新'

--- src/test/java/cn/ayulong/emqdemo/Test02.java
+++ src/test/java/cn/ayulong/emqdemo/Test02.java
@@ -5,5 +5,6 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("test02...");
         System.out.println("C1客户端操作->test02...");
+        System.out.println("C1再次更新汪汪汪");
     }
 }
