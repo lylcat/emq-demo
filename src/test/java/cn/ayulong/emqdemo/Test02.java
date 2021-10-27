3c6741f762c8d0a515b9895c241360f55738b342
阿雨隆 <8080011+ayulong@user.noreply.gitee.com>
2021-10-27 10:16:53 +0800

    C1做出了告别

--- src/test/java/cn/ayulong/emqdemo/Test02.java
+++ src/test/java/cn/ayulong/emqdemo/Test02.java
@@ -7,6 +7,7 @@ public class Test02 {
         System.out.println("C2客户端操作");
         System.out.println("C2再次更新喵喵喵");
         System.out.println("C1：更新个棒棒锤，我代码都没了");
+        System.out.println("C1：撒悠啦啦，阔弄瓦赛狗大");
     }
 }
 
