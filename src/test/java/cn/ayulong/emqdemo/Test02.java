b0c1c48bdc1ed1333d2bda30f9429717d4f6469a
阿雨隆 <8080011+ayulong@user.noreply.gitee.com>
2021-10-27 09:55:06 +0800

    C2再次更新

--- src/test/java/cn/ayulong/emqdemo/Test02.java
+++ src/test/java/cn/ayulong/emqdemo/Test02.java
@@ -5,5 +5,6 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("test02...");
         System.out.println("C2客户端操作");
+        System.out.println("C2再次更新喵喵喵");
     }
 }
