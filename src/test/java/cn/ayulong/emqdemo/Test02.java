a4518b00d7da7279f9df92feef7fcfba147c6228
阿雨隆 <8080011+ayulong@user.noreply.gitee.com>
2021-10-27 10:15:11 +0800

    'C2作出了告别'

--- src/test/java/cn/ayulong/emqdemo/Test02.java
+++ src/test/java/cn/ayulong/emqdemo/Test02.java
@@ -8,5 +8,6 @@ public class Test02 {
         System.out.println("C2再次更新喵喵喵");
         System.out.println("C1：更新个棒棒锤，我代码都没了");
         System.out.println("C2：我他么忘记pull了呀，可恶");
+        System.out.println("C2：这是最后一次了，再见！");
     }
 }
